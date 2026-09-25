b3953d16e90cb7898f27239493b6f4365f51a3bd
Stilll645 <lanyakardou@gmail.com>
2026-09-25 11:40:39 +0200

    Update

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -33,52 +33,87 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  
- fill(255);
- strokeWeight(5);
- rect(vak1X + 100, vak1Y + 320, 100, 40, 10);
- fill(0);
- textSize(20);
- text("Restart?",142, 425 );
+
+  if (player == 1) { //If it's player 1's turn then the backdrop turns red
+    background(184, 0, 0); //Red
+  } else { //If it's anything other than player 1's turn then it's blue
+    background(0, 51, 184); //Blue
+  }
+
+  if (winnerPlayer == 0) {
+    if (player == 1) {
+      fill(255);
+      strokeWeight(5);
+      rect(vak1X + 50, vak1Y - 55, 200, 40, 10);
+      fill(0);
+      text("Player 1's Turn!", 110, 50);
+    } else {
+      fill(255);
+      strokeWeight(5);
+      rect(vak1X + 50, vak1Y - 55, 200, 40, 10);
+      fill(0);
+      text("Player 2's Turn!", 110, 50);
+
+    }
+  }
+
+  fill(255);
+  strokeWeight(5);
+  rect(vak1X + 100, vak1Y + 320, 100, 40, 10);
+  fill(0);
+  textSize(20);
+  text("Restart?", 142, 425);
 
   if (    //Here im making it so that if all the boxes aren't equal to 0 then there'd be a draw
-  vak1colour != 0 &&
-  vak2colour != 0 &&
-  vak3colour != 0 &&
-  vak4colour != 0 &&
-  vak5colour != 0 &&
-  vak6colour != 0 &&
-  vak7colour != 0 &&
-  vak8colour != 0 &&
-  vak9colour != 0 
-  )  if (winner == false) { //Tells my program theres no winner
-    fill(0,126,50);
-    textSize(30);
-    text("It's a Draw...", 50, 40);
+    vak1colour != 0 &&
+    vak2colour != 0 &&
+    vak3colour != 0 &&
+    vak4colour != 0 &&
+    vak5colour != 0 &&
+    vak6colour != 0 &&
+    vak7colour != 0 &&
+    vak8colour != 0 &&
+    vak9colour != 0
+  ) 
+  {
+    // if (winnerPlayer == 3 ) { //Tells my program theres no winner
+      winnerPlayer = 3;  
+      fill(255);
+      strokeWeight(5);
+      rect(vak1X + 10, vak1Y - 70, 200, 40, 10);
+      fill(0, 126, 50);
+      textSize(30);
+      text("It's a Draw...", 50, 40);
+    // }
   }
 
-  if (winner == true ) {
+  if (winnerPlayer > 0) {
     if (winnerPlayer == 1) {
-    fill(0,126,50);
-    textSize(30);
-    text("Player " + winnerPlayer +  " has Won!", 50, 40);
-    if(applause.isPlaying() == false) //Always there to give applause
-    {
-      applause.play();
-    }
+      fill(255);
+      strokeWeight(5);
+      rect(vak1X + 10, vak1Y - 70, 270, 40, 10);
+      fill(0, 126, 50);
+      textSize(30);
+      text("Player " + winnerPlayer + " has Won!", 50, 40);
+      if (applause.isPlaying() == false) //Always there to give applause
+      {
+        applause.play();
+      }
     }
 
-    if (winnerPlayer == 2){
-    fill(0,126,50);
-    textSize(30);
-    text("Player " + winnerPlayer +  " has Won!", 50, 40);
-     if(applause.isPlaying() == false) //Always there to give applause
-    {
-      applause.play();
+    if (winnerPlayer == 2) {
+      fill(255);
+      strokeWeight(5);
+      rect(vak1X + 10, vak1Y - 70, 270, 40, 10);
+      fill(0, 126, 50);
+      textSize(30);
+      text("Player " + winnerPlayer + " has Won!", 50, 40);
+      if (applause.isPlaying() == false) //Always there to give applause
+      {
+        applause.play();
+      }
     }
   }
-}
   // ( rectangles )
   fill(0);
   rect(vak1X, vak1Y, 300, 300, 10);
@@ -180,7 +215,7 @@ function draw() {
   rect(vak1X + 110, vak1Y + 200, 80, 80, 5);
 
   //Box9
-  if (vak9colour == 0) { 
+  if (vak9colour == 0) {
     fill(255) //white
   }
   else if (vak9colour == 1) {
@@ -189,7 +224,7 @@ function draw() {
   else if (vak9colour == 2) {
     fill(0, 51, 184); //blue
   }
- rect(vak1X + 200, vak1Y + 200, 80, 80, 5);
+  rect(vak1X + 200, vak1Y + 200, 80, 80, 5);
 
 }
 
@@ -203,99 +238,99 @@ function changeTurn() { //It changes the turn for the 2 players
 }
 
 function checkWinner() {
- if (
- vak1colour == vak2colour && //basically asking does box 1 belong to the same player
- vak2colour == vak3colour &&
- vak1colour != 0 //is box 1 NOT empty WIN 1
- ) {
-  console.log('winner '+ vak1colour) //checking in consolelog if it works
-  winner = true; //im saying someone has won
-  winnerPlayer = vak1colour; //takes whatever number is currently inside vak1colour and copy it into winnerPlayer
- } 
- if (
- vak4colour == vak5colour &&  //WIN 2
- vak5colour == vak6colour &&
- vak4colour != 0
- ) {
-  console.log('winner '+ vak4colour)
-  winner = true; 
-  winnerPlayer = vak4colour; 
- } 
- if (
- vak7colour == vak8colour && // WIN3
- vak8colour == vak9colour && 
- vak7colour != 0
- ) {
-  console.log('winner '+ vak7colour)
-  winner = true; //
-  winnerPlayer = vak7colour; 
- } 
- if (
- vak1colour == vak4colour && // WIN4
- vak4colour == vak7colour && 
- vak1colour != 0
- ) {
-  console.log('winner '+ vak1colour)
-  winner = true; 
-  winnerPlayer = vak1colour; 
- } 
   if (
- vak2colour == vak5colour && // WIN5
- vak5colour == vak8colour && 
- vak2colour != 0
- ) {
-  console.log('winner '+ vak2colour)
-  winner = true; 
-  winnerPlayer = vak2colour; 
- } 
+    vak1colour == vak2colour && //basically asking does box 1 belong to the same player
+    vak2colour == vak3colour &&
+    vak1colour != 0 //is box 1 NOT empty WIN 1
+  ) {
+    console.log('winner ' + vak1colour) //checking in consolelog if it works
+    winner = true; //im saying someone has won
+    winnerPlayer = vak1colour; //takes whatever number is currently inside vak1colour and copy it into winnerPlayer
+  }
   if (
- vak3colour == vak6colour && // WIN6
- vak6colour == vak9colour && 
- vak3colour != 0
- ) {
-  console.log('winner '+ vak3colour)
-  winner = true; 
-  winnerPlayer = vak3colour; 
- } 
+    vak4colour == vak5colour &&  //WIN 2
+    vak5colour == vak6colour &&
+    vak4colour != 0
+  ) {
+    console.log('winner ' + vak4colour)
+    winner = true;
+    winnerPlayer = vak4colour;
+  }
   if (
- vak1colour == vak5colour && // WIN7
- vak5colour == vak9colour && 
- vak1colour != 0
- ) {
-  console.log('winner '+ vak1colour)
-  winner = true; 
-  winnerPlayer = vak1colour; 
- } 
+    vak7colour == vak8colour && // WIN3
+    vak8colour == vak9colour &&
+    vak7colour != 0
+  ) {
+    console.log('winner ' + vak7colour)
+    winner = true; //
+    winnerPlayer = vak7colour;
+  }
   if (
- vak3colour == vak5colour && // WIN8
- vak5colour == vak7colour && 
- vak3colour != 0
- ) {
-  console.log('winner '+ vak3colour)
-  winner = true; 
-  winnerPlayer = vak3colour; 
- } 
+    vak1colour == vak4colour && // WIN4
+    vak4colour == vak7colour &&
+    vak1colour != 0
+  ) {
+    console.log('winner ' + vak1colour)
+    winner = true;
+    winnerPlayer = vak1colour;
+  }
+  if (
+    vak2colour == vak5colour && // WIN5
+    vak5colour == vak8colour &&
+    vak2colour != 0
+  ) {
+    console.log('winner ' + vak2colour)
+    winner = true;
+    winnerPlayer = vak2colour;
+  }
+  if (
+    vak3colour == vak6colour && // WIN6
+    vak6colour == vak9colour &&
+    vak3colour != 0
+  ) {
+    console.log('winner ' + vak3colour)
+    winner = true;
+    winnerPlayer = vak3colour;
+  }
+  if (
+    vak1colour == vak5colour && // WIN7
+    vak5colour == vak9colour &&
+    vak1colour != 0
+  ) {
+    console.log('winner ' + vak1colour)
+    winner = true;
+    winnerPlayer = vak1colour;
+  }
+  if (
+    vak3colour == vak5colour && // WIN8
+    vak5colour == vak7colour &&
+    vak3colour != 0
+  ) {
+    console.log('winner ' + vak3colour)
+    winner = true;
+    winnerPlayer = vak3colour;
+  }
 }
 
 function mousePressed() { //Making sure the mouse press only counts inside the boxes i want
- 
- 
-  if (mouseX > vak1X + 100 && mouseX < vak1X + 200)  //Makes sure u can click the restart button
-  if (mouseY > vak1Y + 320 && mouseY < vak1Y + 360) {
-    vak1colour = 0; 
-    vak2colour = 0;
-    vak3colour = 0; 
-    vak4colour = 0;
-    vak5colour = 0; 
-    vak6colour = 0;
-    vak7colour = 0; 
-    vak8colour = 0;
-    vak9colour = 0; 
-    winner = false;
-    winnerPlayer = 0;
-    player = 1;
 
- }
+
+  if (mouseX > vak1X + 100 && mouseX < vak1X + 200)  //Makes sure u can click the restart button
+    if (mouseY > vak1Y + 320 && mouseY < vak1Y + 360) {
+      vak1colour = 0;
+      vak2colour = 0;
+      vak3colour = 0;
+      vak4colour = 0;
+      vak5colour = 0;
+      vak6colour = 0;
+      vak7colour = 0;
+      vak8colour = 0;
+      vak9colour = 0;
+      winner = false;
+      winnerPlayer = 0;
+      player = 1;
+
+    }
 
 
   //Box1
@@ -350,7 +385,7 @@ function mousePressed() { //Making sure the mouse press only counts inside the b
   }
   //Box6
   if (mouseX > vak1X + 200 && mouseX < vak1X + 200 + vakSize) {
-    if (mouseY > vak1Y + 110 && mouseY < vak1Y +110 + vakSize) {
+    if (mouseY > vak1Y + 110 && mouseY < vak1Y + 110 + vakSize) {
       if (winner == false && vak6colour == 0) {
         vak6colour = player;
         changeTurn();
